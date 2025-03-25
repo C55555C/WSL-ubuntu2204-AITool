b4727583f27825aeac42e4e7416c6745aac5c6de
C55555C <8667169@gmail.com>
2025-03-26 02:57:56 +0800

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-function info()  { echo -e "\033[1;34m[INFO]\033[0m $1"; }
-function warn()  { echo -e "\033[1;33m[WARN]\033[0m $1"; }
-function error() { echo -e "\033[1;31m[ERROR]\033[0m $1"; }
-function ok()    { echo -e "\033[1;32m[OK]\033[0m $1"; }
+function print_info()  { echo -e "\033[1;34m[INFO]\033[0m $1"; }
+function print_warn()  { echo -e "\033[1;33m[WARN]\033[0m $1"; }
+function print_error() { echo -e "\033[1;31m[ERROR]\033[0m $1"; }
+function print_ok()    { echo -e "\033[1;32m[OK]\033[0m $1"; }
