24a8e309b664184bab0b78c06d92e09894ecb37c
C55555C <8667169@gmail.com>
2025-03-26 03:27:48 +0800

    Update common.sh

--- scripts/common.sh
+++ scripts/common.sh
@@ -1,5 +1,3 @@
-# 更新 common.sh 文件
-cat > ./scripts/common.sh << 'EOF'
 #!/bin/bash
 function print_info()  { echo -e "\033[1;34m[INFO]\033[0m $1"; }
 function print_warn()  { echo -e "\033[1;33m[WARN]\033[0m $1"; }
