0f86a45d1964764e8c5f68e7948cc5648282db5d
C55555C <8667169@gmail.com>
2025-03-26 00:56:50 +0800

    Delete scripts directory

--- scripts/anythingllm/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source ../common.sh
-clear
-info "克隆并安装 AnythingLLM"
-git clone https://github.com/Mintplex-Labs/anything-llm.git
-cd anything-llm && cp .env.example .env
-ok "完成克隆和配置 .env"
-read -p "按回车键返回菜单..." _
-bash ../../wsl-ultimate-deploy.sh
